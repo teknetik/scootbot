0caf62b90ae164a4b6e6f8c5f1b34b2455649dba
Ubuntu <draper.carl@gmail.com>
2022-05-22 18:57:28 +0100

    auto push from scoot-motor

--- scripts/motor/start.sh
+++ scripts/motor/start.sh
@@ -1,5 +1,6 @@
 source ~/scootbot/install/setup.sh
 sudo chmod 777 /dev/ttyS0
+sudo chmod 777 /dev/input/event0
 ros2 run sabertooth drive &
 ros2 run differential_drive twist_to_motors &
 ros2 launch teleop_twist_joy teleop-launch.py joy_config:='xbox' &
