0966b93e1c301ce30d8332704d4c79a79999c602
teknetik <draper.carl@gmail.com>
2022-08-16 22:33:19 +0100

    initial commit

---
+++ build/sllidar_ros2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/scootbot/src/sllidar_ros2
+# Build directory: /home/ubuntu/scootbot/build/sllidar_ros2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
