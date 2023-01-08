b26418def953fdeb17d12cbab49f15f806052313
teknetik <draper.carl@gmail.com>
2023-01-08 16:49:50 +0000

    version3: working

--- 3.0.0/src/scootbot/launch/scootbot_sim.launch.py
+++ 3.0.0/src/scootbot/launch/scootbot_sim.launch.py
@@ -27,8 +27,6 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_share = launch_ros.substitutions.FindPackageShare(package=package_name).find(package_name)
-    default_model_path = pkg_share + '/description/scootbot.urdf'
-    robot_description_config = xacro.process_file(default_model_path)
     default_rviz_config_path = os.path.join(pkg_share, 'config/scootbot.rviz')
 
     rsp = IncludeLaunchDescription(
@@ -65,26 +63,33 @@ def generate_launch_description():
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    worldfile = os.path.join(get_package_share_directory(package_name),'worlds','obstacles.world')
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file, 'world':worldfile}.items()
              )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'scootbot'],
                         output='screen')
 
+
+
     # Launch!
     return LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                            description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
                                             description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument('joy_config', default_value='ps3'),
-        launch.actions.DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0'),
+        launch.actions.DeclareLaunchArgument('joy_dev', default_value='/dev/input/js1'),
         launch.actions.DeclareLaunchArgument('config_filepath', default_value=[
             launch.substitutions.TextSubstitution(text=os.path.join(
-                get_package_share_directory('teleop_twist_joy'), 'config', '')),
+                get_package_share_directory('scootbot'), 'config', '')),
             joy_config, launch.substitutions.TextSubstitution(text='.config.yaml')]),
         DeclareLaunchArgument(
             'use_sim_time',
@@ -111,5 +116,5 @@ def generate_launch_description():
         joint_state_publisher_gui_node,
         rviz_node,
         gazebo,
-        spawn_entity,
+        spawn_entity
     ])
