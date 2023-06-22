031f11c2a51b3a8a985fda1dbfc8f69490de8368
teknetik <draper.carl@gmail.com>
2023-06-22 11:15:30 +0100

    start script

--- 3.0.0/start.sh
+++ 3.0.0/start.sh
@@ -4,7 +4,8 @@ source install/setup.sh
 # SIM
 #ros2 launch scootbot scootbot_sim.launch.py use_ros2_control:=true use_sim_time:=true & 
 # REAL
-ros2 launch scootbot scootbot_robot.launch.py use_ros2_control:=true use_sim_time:=false & 
+ros2 launch scootbot scootbot_robot.launch.py use_ros2_control:=true use_sim_time:=false > log/launch.log 2>&1 &
+sleep 5
 echo "publishing caster_frame_l_link"
 ros2 run tf2_ros static_transform_publisher -0.22 0.165 0.025 0 0 0 1 base_link caster_frame_l_link &
 echo "publishing caster_frame_r_link"
@@ -16,12 +17,12 @@ ros2 run tf2_ros static_transform_publisher -0.25 -0.165 -0.08 0 0 0 1 base_link
 
 
 # Create Map
-ros2 launch scootbot online_async_launch.py use_sim_time:=true &
+ros2 launch scootbot online_async_launch.py use_sim_time:=false > log/slam.log 2>&1 &
 # With Map
 #ros2 launch scootbot slam_localization_launch.py use_sim_time:=true &
-
-
-ros2 launch scootbot localization_launch.py use_sim_time:=true &
 sleep 5
 
-ros2 launch scootbot humble_navigation_launch.py use_sim_time:=true &
+ros2 launch scootbot localization_launch.py use_sim_time:=false > log/localization.log 2>&1 &
+sleep 5
+
+ros2 launch scootbot humble_navigation_launch.py use_sim_time:=false > log/nav2.log 2>&1 &
